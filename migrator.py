8f92e9c4f3352d6286eb98ae36e24870a5ab2f27
Zack Marvel <zpmarvel@gmail.com>
2022-03-06 21:25:43 -0700

    WIP migrator
    It was important to specify 'service' in the migrate request--this
    enabled migration of the more advanced features, like issues.

--- migrator.py
+++ migrator.py
@@ -4,7 +4,9 @@ from argparse import ArgumentParser
 
 
 def get(host, path, params=None, headers=None):
-    response = requests.get(f'http://{host}/api/v1/{path}', headers=headers, params=params)
+    url = f'http://{host}/api/v1/{path}'
+    print(f'GET {url}')
+    response = requests.get(url, headers=headers, params=params)
     json_content = json.loads(response.content)
     return json_content
 
@@ -18,40 +20,48 @@ def delete(host, path, params=None, headers=None):
     # return json_content
 
 
+def post(host, path, **kwargs):
+    url = f'http://{host}/api/v1/{path}'
+    print(f'POST {url}')
+    response = requests.post(url, **kwargs)
+    return response
+
 
 def migrate(src_host, src_token, dst_host, dst_token):
+    src_headers = {'Authorization': f'token {src_token}'}
+    dst_headers = {'Authorization': f'token {dst_token}'}
     # Get user ids
-    src_uid = get(src_host, 'user', headers={'Authorization': f'token {src_token}'})['id']
+    src_uid = get(src_host, 'user', headers=src_headers)['id']
     print('src_uid', src_uid)
-    dst_uid = get(dst_host, 'user', headers={'Authorization': f'token {dst_token}'})['id']
+    dst_uid = get(dst_host, 'user', headers=dst_headers)['id']
     print('dst_uid', dst_uid)
 
-
-    src_repos = get(src_host, 'repos/search',
-            headers={'Authorization': f'token {src_token}'},
-            params={'uid': src_uid})['data']
+    src_repos = get(src_host,
+                    'repos/search',
+                    headers=src_headers,
+                    params={'uid': src_uid})['data']
     for repo in src_repos:
         repo_addr = f'http://{src_host}/{repo["full_name"]}.git'
         print(repo_addr)
-        response = requests.post(f'http://{dst_host}/api/v1/repos/migrate/',
-                headers={'Authorization': f'token {dst_token}'},
-                params={
-                    'access_token': dst_token,
-                    },
-                data={
-                    'auth_token': src_token,
-                    'clone_addr': repo_addr,
-                    'repo_name': repo['name'],
-                    'mirror': repo['mirror'],
-                    'mirror_interval': repo['mirror_interval'],
-                    'description': repo['description'],
-                    'issues': repo['has_issues'],
-                    'pull_requests': repo['has_pull_requests'],
-                    'wiki': repo['has_wiki'],
-                    'private': repo['private'],
-                    })
+        response = post(dst_host,
+                        'repos/migrate',
+                        headers=dst_headers,
+                        data={
+                            'auth_token': src_token,
+                            'clone_addr': repo_addr,
+                            'repo_name': repo['name'],
+                            'mirror': repo['mirror'],
+                            'mirror_interval': repo['mirror_interval'],
+                            'description': repo['description'],
+                            'issues': repo['has_issues'],
+                            'pull_requests': repo['has_pull_requests'],
+                            'wiki': repo['has_wiki'],
+                            'service': 'gitea',
+                            'private': repo['private'],
+                        })
         print(response.status_code)
         print(response.content)
+        response.raise_for_status()
 
         # migrate issues
 
@@ -60,35 +70,44 @@ def migrate(src_host, src_token, dst_host, dst_token):
         # TODO: migreate pull requests
 
 
-
 def delete_all(host, token):
     auth_headers = {'Authorization': f'token {token}'}
     user = get(host, 'user', headers=auth_headers)
     print('user', user)
     uid = user['id']
 
-    repos = get(host, 'repos/search',
-            headers=auth_headers,
-            params={'uid': uid})['data']
+    repos = get(host,
+                'repos/search',
+                headers=auth_headers,
+                params={'uid': uid})['data']
 
     for repo in repos:
-        response = delete(host, f'repos/{user["login"]}/{repo["name"]}', headers=auth_headers)
+        response = delete(host,
+                          f'repos/{user["login"]}/{repo["name"]}',
+                          headers=auth_headers)
         print(response.status_code)
         print(response.content)
-
+        response.raise_for_status()
 
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser(description='Migrate from one gitea instance to another')
+    parser = ArgumentParser(
+        description='Migrate from one gitea instance to another')
 
     parser.add_argument('--src-token', help='Source API token')
-    parser.add_argument('--src-token-file', help='File containing source API token')
+    parser.add_argument('--src-token-file',
+                        help='File containing source API token')
     parser.add_argument('--dst-token', help='Destination API token')
-    parser.add_argument('--dst-token-file', help='File containing destination API token')
-    parser.add_argument('--delete-all', action='store_true', help='Instead of migrating, delete all repos')
-    parser.add_argument('src_host', help='Address (and optionally port) of source host')
-    parser.add_argument('dst_host', help='Address (and optionally port) of destination host')
+    parser.add_argument('--dst-token-file',
+                        help='File containing destination API token')
+    parser.add_argument('--delete-all',
+                        action='store_true',
+                        help='Instead of migrating, delete all repos')
+    parser.add_argument('src_host',
+                        help='Address (and optionally port) of source host')
+    parser.add_argument(
+        'dst_host', help='Address (and optionally port) of destination host')
 
     args = parser.parse_args()
 
